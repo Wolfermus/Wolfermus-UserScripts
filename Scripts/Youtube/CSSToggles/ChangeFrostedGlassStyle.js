26d2c49ca40d34b9c4d2e6489ece1114a6cbd217
Feb199 <32810568+feb199@users.noreply.github.com>
2025-06-01 10:46:31 +0100

    Scripts::Youtube::CSSToggles - Added
    Added ability to change frosted glass background color

---
+++ Scripts/Youtube/CSSToggles/ChangeFrostedGlassStyle.js
@@ -0,0 +1,74 @@
+/**
+ * @returns {HTMLStyleElement}
+ */
+function WolfermusYoutubeGetSetStyleElement() {
+    const wlfYoutubeStyleElementIDName = "wlf-youtube-style-overrides";
+
+    let wlfYoutubeStyleElement = document.getElementById(wlfYoutubeStyleElementIDName);
+
+    if (wlfYoutubeStyleElement === null) {
+        wlfYoutubeStyleElement = document.createElement('style');
+        wlfYoutubeStyleElement.id = wlfYoutubeStyleElementIDName;
+        document.head.append(wlfYoutubeStyleElement);
+    }
+
+    return wlfYoutubeStyleElement;
+}
+
+async function WolfermusYoutubeChangeFrostedGlassStyle() {
+    /**
+     * @param {number | undefined} ms
+     */
+    function Sleep(ms) {
+        return new Promise(resolve => {
+            setTimeout(resolve, ms)
+        });
+    }
+
+    let gottenElement = document.getElementById("frosted-glass");
+    if (gottenElement === null) return;
+
+    // Get the current background-color value:
+    let value = getComputedStyle(gottenElement).getPropertyValue("background-color");
+    console.log(value);
+
+    // Get all color components (alpha may not be there if = 1):
+    let parts = value.match(/[\d.]+/g);
+
+    // If alpha is not there, add it:
+    if (parts.length === 3) {
+        parts.push(1);
+    }
+
+    while (parts[0] === "0" && parts[1] === "0" && parts[2] === "0") {
+        await Sleep(500);
+
+        parts = null;
+
+        gottenElement = document.getElementById("frosted-glass");
+        if (gottenElement === null) return;
+
+        // Get the current background-color value:
+        value = getComputedStyle(gottenElement).getPropertyValue("background-color");
+        console.log(value);
+
+        // Get all color components (alpha may not be there if = 1):
+        parts = value.match(/[\d.]+/g);
+
+        // If alpha is not there, add it:
+        if (parts.length === 3) {
+            parts.push(1);
+        }
+    }
+
+    // Modify alpha:
+    parts[3] = "1";
+
+    WolfermusYoutubeGetSetStyleElement().textContent = `
+    #frosted-glass.with-chipbar.ytd-app {
+        background: rgba(${parts.join(',')});
+    }
+    `;
+}
+
+WolfermusYoutubeChangeFrostedGlassStyle();
---
+++ Scripts/Youtube/CSSToggles/RestoreFrostedGlassBackgroundColor.js
@@ -0,0 +1,18 @@
+/**
+ * @returns {HTMLStyleElement}
+ */
+function WolfermusYoutubeGetSetStyleElement() {
+    const wlfYoutubeStyleElementIDName = "wlf-youtube-style-overrides";
+
+    let wlfYoutubeStyleElement = document.getElementById(wlfYoutubeStyleElementIDName);
+
+    if (wlfYoutubeStyleElement === null) {
+        wlfYoutubeStyleElement = document.createElement('style');
+        wlfYoutubeStyleElement.id = wlfYoutubeStyleElementIDName;
+        document.head.append(wlfYoutubeStyleElement);
+    }
+
+    return wlfYoutubeStyleElement;
+}
+
+WolfermusYoutubeGetSetStyleElement().textContent = ``;
