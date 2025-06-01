f2573f02c342d74b41fbd24f6e46a6b4a47546c5
Feb199 <32810568+feb199@users.noreply.github.com>
2025-06-01 11:51:45 +0100

    Scripts - Added Main.js

---
+++ Scripts/Main.js
@@ -0,0 +1,75 @@
+(async () => {
+    let IsGMXmlHttpRequest1 = false;
+    // @ts-ignore
+    if (typeof GM_xmlHttpRequest !== "undefined" && typeof GM_xmlHttpRequest !== "null" && GM_xmlHttpRequest) IsGMXmlHttpRequest1 = true;
+
+    let IsGMXmlHttpRequest2 = false;
+    // @ts-ignore
+    if (typeof GM !== "undefined" && typeof GM.xmlHttpRequest !== "undefined") IsGMXmlHttpRequest2 = true;
+
+    let IsGMXmlHttpRequest = false;
+    if (IsGMXmlHttpRequest1 || IsGMXmlHttpRequest2) IsGMXmlHttpRequest = true;
+
+    if (!IsGMXmlHttpRequest) {
+        const message = "Wolfermus ERROR: Youtube CSSToggles - Please run in a userscript manager";
+        alert(message);
+        console.log(message);
+        return;
+    }
+
+    let ChosenXmlHttpRequest;
+    if (IsGMXmlHttpRequest2) {
+        ChosenXmlHttpRequest = GM.xmlHttpRequest;
+    } else if (IsGMXmlHttpRequest1) {
+        ChosenXmlHttpRequest = GM_xmlHttpRequest;
+    } else {
+        const message = "Wolfermus ERROR: Youtube CSSToggles - Unexpected Error";
+        alert(message);
+        console.log(message);
+        return;
+    }
+    if (ChosenXmlHttpRequest === undefined || ChosenXmlHttpRequest === null) {
+        const message = "Wolfermus ERROR: Youtube CSSToggles - Unexpected Error";
+        alert(message);
+        console.log(message);
+        return;
+    }
+
+    function MakeGetRequest(url) {
+        return new Promise((resolve, reject) => {
+            ChosenXmlHttpRequest({
+                method: "GET",
+                url: url,
+                onload: (response) => {
+                    if (response.status !== 200) {
+                        reject(statusText);
+                        return;
+                    }
+                    resolve(response.responseText);
+                },
+                onerror: error => reject(error)
+            });
+        });
+    }
+
+    const bypassScriptPolicy = trustedTypes.createPolicy("bypassScript", {
+        createScript: (string) => string,
+        createScriptURL: (string) => string
+    });
+
+    /**
+     * @param {number | undefined} ms
+     */
+    function Sleep(ms) {
+        return new Promise(resolve => {
+            setTimeout(resolve, ms)
+        });
+    }
+
+    async function LoadScript(name) {
+        const script = bypassScriptPolicy.createScript(await MakeGetRequest(`https://raw.githubusercontent.com/Wolfermus/Wolfermus-UserScripts/refs/heads/main/Scripts/${name}/Main.js`));
+        eval(script);
+    }
+
+    await LoadScript("Youtube");
+})();
