f57816a638735109c616de6c7c339ad19a9e593c
Feb199 <32810568+feb199@users.noreply.github.com>
2025-06-01 15:30:49 +0100

    Locating Bug

--- Scripts/Youtube/CSSToggles/Main.js
+++ Scripts/Youtube/CSSToggles/Main.js
@@ -1,6 +1,6 @@
 async () => {
     debugger;
-    if (window.location.href !== "https://www.youtube.com") return;
+    if (window.location.href !== "https://www.youtube.com/") return;
 
     let IsGMXmlHttpRequest1 = false;
     // @ts-ignore
