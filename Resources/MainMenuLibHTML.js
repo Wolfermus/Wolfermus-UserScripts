12f156f0c06f715ada8e2cae53f1b622cff0e47a
Feb199 <32810568+feb199@users.noreply.github.com>
2025-06-05 07:38:54 +0100

    Resources::MainMenuLibHTML - Added
    Resources::MainMenuLibHTML - Added `MainMenuLibHTML.js` which is html extracted from MainMenuLib.

---
+++ Resources/MainMenuLibHTML.js
@@ -0,0 +1,28 @@
+`
+<span id="WolfermusMenuItemToolTip" class="WolfermusPopUpMenu WolfermusText">
+    <a id="WolfermusToolTipText">
+    </a>
+</span>
+<div id="WolfermusMainMenuContextMenu" class="WolfermusPopUpMenu WolfermusText">
+    <ul class="WolfermusDefaultCSS">
+        ${GenerateContextItems()}
+    </ul>
+</div>
+<div id="WolfermusFloatingSnapBtnWrapper"
+    class="WolfermusDefaultCSS ${WolfermusMainMenuSettings.Direction?.Horizontal ? WolfermusMainMenuSettings.Direction?.Horizontal : ""} ${WolfermusMainMenuSettings.Direction?.Vertical ? WolfermusMainMenuSettings.Direction?.Vertical : ""}"
+    style="${WolfermusMainMenuSettings.Top ? " top: " + WolfermusMainMenuSettings.Top + " px;" : ""}
+    ${WolfermusMainMenuSettings.Left ? "left: " + WolfermusMainMenuSettings.Left + "px;" : ""}">
+    <!-- BEGIN :: Floating Button -->
+    <div id="WolfermusFloatingSnapBtn" class="WolfermusFabBtn WolfermusDefaultCSS WolfermusPopUpMenu">
+        <img src="${imgSrc}" class="WolfermusDefaultCSS"></img>
+    </div>
+    <!-- END :: Floating Button -->
+    <!-- BEGIN :: Expand Section -->
+    <div class="WolfermusActivatedMainMenuWindow WolfermusDefaultCSS WolfermusPopUpMenu WolfermusText">
+        <ul class="WolfermusDefaultCSS">
+            ${GenerateMenuItems()}
+        </ul>
+    </div>
+    <!-- END :: Expand Section -->
+</div>
+`
