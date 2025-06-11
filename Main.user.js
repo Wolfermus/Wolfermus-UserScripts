eea3df6010b4a4af4b8d59c8f483dfecc47b0ac5
Feb199 <32810568+feb199@users.noreply.github.com>
2025-06-11 13:40:10 +0100

    Main - Updated Version

--- Main.user.js
+++ Main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wolfermus Main Menu
 // @namespace    https://greasyfork.org/en/users/900467-feb199
-// @version      2.0.3
+// @version      2.0.4
 // @description  This script is a main menu that loads displays all scripts and allows you to enable them.
 // @author       Feb199/Dannysmoka
 // @homepageURL  https://github.com/Wolfermus/Wolfermus-UserScripts
