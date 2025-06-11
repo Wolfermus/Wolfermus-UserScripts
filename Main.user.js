c73e0057a919a58f76f1cb3ec5537f4795ca9189
Feb199 <32810568+feb199@users.noreply.github.com>
2025-06-11 12:42:14 +0100

    Main - Commented out unnecessary Sleep

--- Main.user.js
+++ Main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wolfermus Main Menu
 // @namespace    https://greasyfork.org/en/users/900467-feb199
-// @version      2.0.2
+// @version      2.0.3
 // @description  This script is a main menu that loads displays all scripts and allows you to enable them.
 // @author       Feb199/Dannysmoka
 // @homepageURL  https://github.com/Wolfermus/Wolfermus-UserScripts
@@ -270,7 +270,7 @@ function WolfermusCheckLibraryLoaded(key) {
         return eval(script)();
     }
 
-    await Sleep(1000);
+    //await Sleep(1000);
 
     let wolfermusPreventLoopLock1 = 10;
 
