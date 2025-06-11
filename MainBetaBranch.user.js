aea483bc28891ff4f58b41093cbb128d880c16fc
Feb199 <32810568+feb199@users.noreply.github.com>
2025-06-11 17:15:11 +0100

    MainBeta - Updated Version

--- MainBetaBranch.user.js
+++ MainBetaBranch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wolfermus Main Menu (Beta)
 // @namespace    https://greasyfork.org/en/users/900467-feb199
-// @version      2.0.8
+// @version      2.0.9
 // @description  This script is a main menu that loads displays all scripts and allows you to enable them. (Beta)
 // @author       Feb199/Dannysmoka
 // @homepageURL  https://github.com/Wolfermus/Wolfermus-UserScripts
