c19a9e2906136f1a772c088fb09cb04e63a7b5e4
Feb199 <32810568+feb199@users.noreply.github.com>
2025-06-11 14:54:02 +0100

    Libraries::MainMenu - Fixed Bug

--- Libraries/MainMenuLib.user.js
+++ Libraries/MainMenuLib.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wolfermus Main Menu Library
 // @namespace    https://greasyfork.org/en/users/900467-feb199
-// @version      2.0.5
+// @version      2.0.6
 // @description  This script is a main menu library that provides easy means to add menu items and manipulate main menu
 // @author       Feb199/Dannysmoka
 // @homepageURL  https://github.com/Wolfermus/Wolfermus-UserScripts
@@ -461,7 +461,7 @@ function GetWolfermusFabImageMenuItem() {
             const fabElement = document.getElementById("WolfermusFloatingSnapBtnWrapper");
             if (fabElement === undefined || fabElement === null) return;
 
-            if (WolfermusFabImageMenuItem.oldPositionY === fabElement.style.top && WolfermusFabImageMenuItem.oldPositionX === fabElement.style.left) {
+            if ((fabElement.style.top === undefined || fabElement.style.left === undefined) || (WolfermusFabImageMenuItem.oldPositionY === fabElement.style.top && WolfermusFabImageMenuItem.oldPositionX === fabElement.style.left)) {
                 GetMainMenu().ToggleVisibility();
             }
         };
--- Resources/MainMenuLibHTML.js
+++ Resources/MainMenuLibHTML.js
@@ -1,6 +1,6 @@
 `
 <div id="WolfermusFloatingSnapBtnWrapper"
-    class="WolfermusDefaultCSS ${WolfermusMainMenuSettings.Direction?.Horizontal ? WolfermusMainMenuSettings.Direction?.Horizontal : ""} ${WolfermusMainMenuSettings.Direction?.Vertical ? WolfermusMainMenuSettings.Direction?.Vertical : ""}"
+    class="WolfermusDefaultCSS ${WolfermusMainMenuSettings.Direction?.Horizontal ? WolfermusMainMenuSettings.Direction?.Horizontal : "WolfermusRightPosition"} ${WolfermusMainMenuSettings.Direction?.Vertical ? WolfermusMainMenuSettings.Direction?.Vertical : "WolfermusDownPosition"}"
     style="${WolfermusMainMenuSettings.Top ? " top: " + WolfermusMainMenuSettings.Top + " px;" : ""}
     ${WolfermusMainMenuSettings.Left ? "left: " + WolfermusMainMenuSettings.Left + "px;" : ""}">
 </div>
